6abaf748cfa1e34d496dd4eb0bc6faeeae099356
danilserov <danil.serov2010@yandex.ru>
2026-01-16 14:02:25 +0300

    Initial

---
+++ RoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace KeycloakAdmin
+{
+  public class RoleConstants
+  {
+    public const string admin = "admin";
+    public const string user = "user";
+    public const string power_user = "power_user";
+  }
+}
